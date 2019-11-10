d9b08200ea5a213383cc116b2b4efde9069b5987
jmbodnar <jmbodnar@protonmail.com>
2019-11-10 12:16:25 -0500

    All working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,22 +1,108 @@
-/* 
-
-6. The app should then prompt users with two messages.
-
-   * The first should ask them the ID of the product they would like to buy.
-   * The second message should ask how many units of the product they would like to buy.
-
-7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-   * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-   * This means updating the SQL database to reflect the remaining quantity.
-   * Once the update goes through, show the customer the total cost of their purchase. 
-
-*/
-
+// ===== Requires & Variables ===== //
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const colors = require('colors');
+
+const orderDetails = [
+  {
+    name: 'item_id',
+    message: 'What is the ID of what you want?'
+  },
+  {
+    name: 'stock',
+    message: 'How many do you want?'
+  }
+];
+
+// ===== Functions ===== //
+
+function startOrders() {
+  inquirer
+    .prompt({
+      type: 'confirm',
+      name: 'continue',
+      message: 'Would you like to buy something?'
+    })
+    .then(response => {
+      if (response.continue) {
+        console.log('\n> Great. Let me show you our inventory:');
+        showInventory();
+      } else {
+        endProgram();
+      }
+    });
+}
+
+function showInventory() {
+  connection.query('SELECT * FROM products', (error, rows) => {
+    if (error) throw error;
+    let inventory = rows
+      .map(row => {
+        return `\nID: ${row.item_id}\tProduct: ${row.product_name}\tDepartment: ${row.department_name}\tPrice: ${row.price}\tStock: ${row.stock}`;
+      })
+      .join('');
+    console.log(colors.green(inventory + '\n'));
+    getOrder();
+  });
+}
+
+function getOrder() {
+  inquirer.prompt(orderDetails).then(orderDetails => {
+    runOrder(orderDetails);
+  });
+}
+
+function runOrder(orderDetails) {
+  connection.query(
+    'UPDATE products SET stock = stock - ? WHERE item_id = ? AND stock >= ?',
+    [orderDetails.stock, orderDetails.item_id, orderDetails.stock],
+    (error, rows) => {
+      // if (error) throw error;
+      if (error) {
+        console.log(
+          colors.red(
+            "\n> That's a bullshit, gibberish order. Let's try this again.\n"
+          )
+        );
+        startOrders();
+      } else if (rows.changedRows === 1) {
+        displayMoneySpent(orderDetails.item_id, orderDetails.stock);
+      } else {
+        console.log(
+          colors.red(
+            "\n> Computer says, 'Insufficient quantity.' We can't sell that many. Let's try this again.\n"
+          )
+        );
+        startOrders();
+      }
+    }
+  );
+}
+
+function displayMoneySpent(item_id, amount_purchased) {
+  connection.query(
+    'SELECT * FROM products WHERE item_id = ?',
+    [item_id],
+    (error, rows) => {
+      if (error) throw error;
+      console.log(
+        '\n> Okay. That comes to $' +
+          Number(Number(rows[0].price) * Number(amount_purchased)).toFixed(2) +
+          ". Let's do this again.\n"
+      );
+      startOrders();
+    }
+  );
+}
+
+function endProgram() {
+  connection.end();
+  return console.log(
+    '\n> Okay. It was nice meeting you. See you some other time.'
+  );
+}
+
+// ===== Inits & Listeners ===== //
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -26,14 +112,4 @@ const connection = mysql.createConnection({
   password: 'docker'
 });
 
-connection.query('SELECT * FROM products', (error, rows) => {
-  if (error) throw error;
-  let inventory = rows
-    .map(row => {
-      return `\nID: ${row.item_id}\tProduct: ${row.product_name}\tDepartment: ${row.department_name}\tPrice: ${row.price}\tStock: ${row.stock}`;
-    })
-    .join('');
-  console.log(inventory);
-});
-
-connection.end();
+startOrders();
